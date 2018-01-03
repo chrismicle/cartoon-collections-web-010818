6f12b71356e7738a0a2b524f47474464b4985d6c
Christina Collura <cmcollura@gmail.com>
2018-01-03 20:25:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(planeteer_calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
